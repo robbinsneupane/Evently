2271b7ac507a63a3204100e00d1bd887903a291d
Rabins <rabinsneupane07@gmail.com>
2024-08-26 10:02:14 +0530

    wrote the code base

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-4xl">hero
+      </h1>
+      <Button>Hello</Button>
+    </main>
+  );
+}
